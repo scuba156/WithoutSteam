74c8853b7dd469cfa6aa20f72fa2280a326eb1bb
scuba156 <scuba156@gmail.com>
2019-10-08 01:00:08 +1100

    - Added null check and now closes the first instance of a steamclientmissing dialog - Bump to version 1.1

--- WithoutSteam/Source/Patches/Dialog_MessageBox_Patches/PostOpen_Patch.cs
+++ WithoutSteam/Source/Patches/Dialog_MessageBox_Patches/PostOpen_Patch.cs
@@ -7,11 +7,13 @@ namespace WithoutSteam.Patches.Dialog_MessageBox_Patches {
     [HarmonyPatch(typeof(Dialog_MessageBox))]
     [HarmonyPatch("PostOpen")]
     public static class PostOpen_Patch {
-
+        private static bool hasAlreadyClosedWindow = false;
         public static void Prefix(this Dialog_MessageBox __instance) {
+            if (hasAlreadyClosedWindow) return;
             string text = "SteamClientMissing".Translate();
-            if (__instance.text == text) {
+            if (!__instance.text.NullOrEmpty() && __instance.text == text) {
                 __instance.Close();
+                hasAlreadyClosedWindow = true;
                 Log.Message("SteamClientMissing Dialog Window closed.");
             }
         }
