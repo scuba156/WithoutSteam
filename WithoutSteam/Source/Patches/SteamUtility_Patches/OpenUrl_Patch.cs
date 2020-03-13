0ba1b9492240ab837208478502adcdae9727e284
scuba156 <scuba156@gmail.com>
2020-03-14 02:39:12 +1100

    Added patch for SteamUtility.OpenUrl to open Steam formatted urls in a browser

---
+++ WithoutSteam/Source/Patches/SteamUtility_Patches/OpenUrl_Patch.cs
@@ -0,0 +1,17 @@
+﻿using Harmony;
+using UnityEngine;
+using Verse;
+using WithoutSteam.Utils;
+
+namespace WithoutSteam.Patches.SteamUtility_Pathces {
+
+    [HarmonyPatch]
+    [HarmonyPatch(typeof(SteamUtility), "OpenUrl")]
+    public static class OpenUrl_Patch {
+
+        public static bool Prefix(string url) {
+            Application.OpenURL(SteamUtils.ConvertFromSteamUrl(url));
+            return false;
+        }
+    }
+}
--- WithoutSteam/Source/Utils/SteamUtils.cs
+++ WithoutSteam/Source/Utils/SteamUtils.cs
@@ -1,7 +1,37 @@
-﻿namespace WithoutSteam.Utils {
+﻿using System;
+using Verse;
+
+namespace WithoutSteam.Utils {
 
     public static class SteamUtils {
         public static readonly string RimWorldAppId = "294100";
         public static readonly string RimWorldSteamWorkshopUrl = "https://steamcommunity.com/workshop/browse/?appid=" + RimWorldAppId;
+        private const string STEAM_APP_URL = "https://store.steampowered.com/app/";
+        private const string STEAM_SHAREDFILE_URL = "https://steamcommunity.com/sharedfiles/filedetails/?id=";
+        private const string STEAM_URL_PREFIX = "steam://url";
+
+        public static string ConvertFromSteamUrl(string url) {
+            if (!url.NullOrEmpty() && url.StartsWith(STEAM_URL_PREFIX, StringComparison.OrdinalIgnoreCase)) {
+                int idStartIndex = url.TrimEnd('/').LastIndexOf('/');
+                int urlTypeStartIndex = STEAM_URL_PREFIX.Length + 1;
+                string id = url.TrimEnd('/').Substring(idStartIndex + 1);
+
+                int urlTypeLength = url.Length - (id.Length + STEAM_URL_PREFIX.Length + 1) - 1;
+                string urlType = url.Substring(urlTypeStartIndex, urlTypeLength);
+
+                switch (urlType) {
+                    case "StoreAppPage":
+                        return STEAM_APP_URL + id;
+
+                    case "CommunityFilePage":
+                        return STEAM_SHAREDFILE_URL + id;
+
+                    default:
+                        Log.Error("WithoutSteam -- Unknown url type: " + urlType);
+                        break;
+                }
+            }
+            return url;
+        }
     }
 }
