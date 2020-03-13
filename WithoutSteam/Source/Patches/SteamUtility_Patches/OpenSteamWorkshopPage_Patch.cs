62bdbddd6a036223166a298aea55cccd791e3200
scuba156 <scuba156@gmail.com>
2020-03-14 02:33:58 +1100

    Added patching of SteamUtility.OpenSteamWorkshopPage

---
+++ WithoutSteam/Source/Patches/SteamUtility_Patches/OpenSteamWorkshopPage_Patch.cs
@@ -0,0 +1,17 @@
+﻿using Harmony;
+using UnityEngine;
+using Verse;
+using WithoutSteam.Utils;
+
+namespace WithoutSteam.Patches.SteamUtility_Pathces {
+
+    [HarmonyPatch]
+    [HarmonyPatch(typeof(SteamUtility), "OpenSteamWorkshopPage")]
+    public static class OpenSteamWorkshopPage_Patch {
+
+        public static bool Prefix() {
+            Application.OpenURL(SteamUtils.RimWorldSteamWorkshopUrl);
+            return false;
+        }
+    }
+}
---
+++ WithoutSteam/Source/Utils/SteamUtils.cs
@@ -0,0 +1,7 @@
+﻿namespace WithoutSteam.Utils {
+
+    public static class SteamUtils {
+        public static readonly string RimWorldAppId = "294100";
+        public static readonly string RimWorldSteamWorkshopUrl = "https://steamcommunity.com/workshop/browse/?appid=" + RimWorldAppId;
+    }
+}
